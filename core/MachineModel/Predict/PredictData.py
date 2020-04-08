9e81b4f7756cb8f2cb2d2faa6f26e53266795c98
bluetoonist <bluetoon@naver.com>
2020-04-08 23:23:15 +0900

    readme.md 업데이트

--- core/MachineModel/Predict/PredictData.py
+++ core/MachineModel/Predict/PredictData.py
@@ -27,33 +27,16 @@ from time import localtime
 
 cur_time = str(localtime(time.time()).tm_min) + ":" + "0" + str(localtime(time.time()).tm_sec)
 
-save_path = str()
+for _ in range(0,6):
+    save_path = "../Models/saved"+str(_)+"/saved"+str(_)+".cpkt"
+    print(save_path)
 
-if len(cur_time.split(":")[0]) == 1:
-    print("0분대")
-    save_path = "../Models/saved0/saved0.cpkt"
-elif len(cur_time.split(":")[0]) == 2:
-    if int(cur_time.split(":")[0][0]) == 1:
-        save_path = "../Models/saved1/saved1.cpkt"
-    elif int(cur_time.split(":")[0][0]) == 2:
-        save_path = "../Models/saved2/saved2.cpkt"
-    elif int(cur_time.split(":")[0][0]) == 3:
-        save_path = "../Models/saved3/saved3.cpkt"
-    elif int(cur_time.split(":")[0][0]) == 4:
-        save_path = "../Models/saved4/saved4.cpkt"
-    elif int(cur_time.split(":")[0][0]) == 5:
-        save_path = "../Models/saved5/saved5.cpkt"
-else:
-    pass
-print(save_path)
+    init = tf.compat.v1.global_variables_initializer()
+    sess = tf.compat.v1.Session()
 
-init = tf.compat.v1.global_variables_initializer()
-saver = tf.compat.v1.train.Saver()
+    saver = tf.compat.v1.train.Saver()
+    saver.restore(sess, save_path)
 
-sess = tf.compat.v1.Session()
-saver.restore(sess,save_path)
-
-for x in range(20):
     all_process = [x.pid for x in psutil.process_iter()]
     thread_count = 0
     for x in range(0, len(all_process)): thread_count += psutil.Process(pid=all_process[x]).num_threads()
@@ -71,6 +54,7 @@ for x in range(20):
 
     returnValue = (memory_usage.percent - predict_memory_usage) % 2
 
-    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
     print("\nCurrent Usage: ", memory_usage.percent, "Predict :",
           sess.run(H, feed_dict={X1: X1_process, X2: X2_threads}), "returnvalue :", returnValue)
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+    time.sleep(1)
